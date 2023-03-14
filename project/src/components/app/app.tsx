8a63e6a781afa7b55e77611aa563c1e176c7c6f0
Dmitriy Borovkov <borovvkovv@gmail.comi>
2023-03-14 19:55:09 +0500

    2.16. Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type AppProps = {
+  placeNum: number;
+};
+
+function App({ placeNum }: AppProps): JSX.Element {
+  return <Main placeNum={placeNum} />;
 }
 
 export default App;
